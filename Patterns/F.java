235972493dfbd5a559b25fe38648d8c8af2dd4f4
SanikaBhandari07 <156589437+SanikaBhandari07@users.noreply.github.com>
2024-11-03 21:19:06 +0530

    Add files via upload

---
+++ Patterns/F.java
@@ -0,0 +1,28 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class F {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of n");
+        int n = sc.nextInt();
+        System.out.println();
+        int mid = (n+1)/2;
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n-1; j++){
+                if(j==1 || i==1 || i== mid ){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+
+        }
+        sc.close();
+    }
+}
